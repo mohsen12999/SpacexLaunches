ae11c7ce3b096bd50f6943ec46a8c04152adf7b9
mohsen1299@gmail.com <mohsen1299@gmail.com>
2023-05-08 12:09:40 +0430

    update navbar and add github link and logo

--- SpacexLaunches/ClientApp/src/components/NavMenu.js
+++ SpacexLaunches/ClientApp/src/components/NavMenu.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Collapse, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import github_svg from '../statics/github.svg'
 import './NavMenu.css';
 
 export class NavMenu extends Component {
@@ -25,18 +26,14 @@ export class NavMenu extends Component {
     return (
       <header>
         <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" container light>
-          <NavbarBrand tag={Link} to="/">SpacexLaunches</NavbarBrand>
+          <NavbarBrand tag={Link} to="/">SpaceX Launches</NavbarBrand>
           <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
           <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
             <ul className="navbar-nav flex-grow">
               <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
+                <NavLink tag={Link} className="text-dark" to="https://github.com/mohsen12999/SpacexLaunches" target='_blank'>
+                  <img src={github_svg} alt='github logo' />
+                </NavLink>
               </NavItem>
             </ul>
           </Collapse>
