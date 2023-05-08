a5452ac6fecf24d27b9ce8f45b4c8fad2424dd68
mohsen1299@gmail.com <mohsen1299@gmail.com>
2023-05-09 03:59:44 +0430

    change home page and add link to launch data

--- SpacexLaunches/ClientApp/src/AppRoutes.js
+++ SpacexLaunches/ClientApp/src/AppRoutes.js
@@ -1,4 +1,4 @@
-import { Home }  from "./components/Home";
+import Home  from "./components/Home";
 import LaunchData from "./components/LaunchData";
 
 const AppRoutes = [
--- SpacexLaunches/ClientApp/src/components/Home.js
+++ SpacexLaunches/ClientApp/src/components/Home.js
@@ -1,64 +1,58 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Link } from "react-router-dom";
+
 import x_svg from '../statics/x.svg'
 import check_svg from '../statics/check.svg'
 
-export class Home extends Component {
-    static displayName = Home.name;
+const Home = () => {
+    const [loading, setLoading] = React.useState(true);
+    const [allLaunchesData, setAllLaunchesData] = React.useState({});
 
-    constructor(props) {
-        super(props);
-        this.state = { launches: [], loading: true };
-    }
+    React.useEffect(() => {
+        getAllLaunchesData();
+    }, []);
 
-    componentDidMount() {
-        this.getLaunchData();
-    }
-
-    static renderLaunchesTable(launches) {
-        console.log(launches);
-        return (
-            <table className="table table-striped" aria-labelledby="tableLabel">
-                <thead>
-                    <tr>
-                        <th>Mission Name</th>
-                        <th>Rocket Name</th>
-                        <th>Site Name</th>
-                        <th>Launch Year</th>
-                        <th>Launch Success</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {launches.map(launch =>
-                        <tr key={launch.flight_number}>
-                            <td>{launch.mission_name}</td>
-                            <td>{launch.rocket.rocket_name}</td>
-                            <td>{launch.launch_site.site_name}</td>
-                            <td>{launch.launch_year}</td>
-                            <td>{launch.launch_success ? <img src={check_svg} alt='check sign' />: <img src={x_svg} alt='x sign' />}</td>
-                        </tr>
-                    )}
-                </tbody>
-            </table>
-        );
-    }
-
-    render() {
-        let contents = this.state.loading
-            ? <p><em>Loading...</em></p>
-            : Home.renderLaunchesTable(this.state.launches);
-
-        return (
-            <div>
-                <h1 id="tableLabel">launches List</h1>
-                {contents}
-            </div>
-        );
-    }
-
-    async getLaunchData() {
+    const getAllLaunchesData = async () => {
         const launches_url = "https://api.spacexdata.com/v3/launches";
         const response = await fetch(launches_url);
         const data = await response.json();
-        this.setState({ launches: data, loading: false });
+        setAllLaunchesData(data);
+        setLoading(false);
     }
+
+    const renderLaunchesTable = (launches) => (
+        <table className="table table-striped table-hover" aria-labelledby="tableLabel">
+            <thead>
+                <tr>
+                    <th>Mission Name</th>
+                    <th>Rocket Name</th>
+                    <th>Site Name</th>
+                    <th>Launch Year</th>
+                    <th>Launch Success</th>
+                </tr>
+            </thead>
+            <tbody>
+                {launches.map(launch =>
+                    <tr key={launch.flight_number}>
+                        <td>
+                            <Link to={"/launches/"+launch.flight_number}>{launch.mission_name}</Link></td>
+                        <td>{launch.rocket.rocket_name}</td>
+                        <td>{launch.launch_site.site_name}</td>
+                        <td>{launch.launch_year}</td>
+                        <td>{launch.launch_success ? <img src={check_svg} alt='check sign' /> : <img src={x_svg} alt='x sign' />}</td>
+                    </tr>
+                )}
+            </tbody>
+        </table>
+    );
+
+    const content = loading ? <p><em>Loading...</em></p> : renderLaunchesTable(allLaunchesData)
+
+    return <div>
+        <h1 id="tableLabel">launches List</h1>
+        {content}
+    </div>;
+
 }
+
+export default Home;
